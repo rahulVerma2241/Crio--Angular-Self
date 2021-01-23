3373d1b741e5cc164b7ae446cb584b73e49abd7d
Rahul Verma <agni5.rv@gmail.com>
2021-01-23 13:47:52 +0000

    Intial commit

---
+++ src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Rahul Kumar",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Rahul Kumar and I'm a Software Engineer!",
+  githubUrl: "https://github.com/ashokkumar95",
+  linkedinUrl: "https://www.linkedin.com/in/rahul-verma-ta",
+  cvLink: "https://docs.google.com/document/d/1NV3RfzSCsBP2A63mviHQm8tFHEwxnotT1UjYiDF8Ba4/edit?usp=sharing",
+};
